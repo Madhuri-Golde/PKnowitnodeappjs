643471128bf214f44e9be84e8b57f2237a61d69c
Madhuri-Golde <madhurigolde95@gmail.com>
2023-02-05 22:43:10 +0530

    connected with database

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
-var express = require('express');
-var app = express();
+var exp = require("express");
+var mysql = require("mysql2");
 
-//Reusable controllers
-var productController=function(req, res){
-  console.log("Invoking  rest api for product list");
-  var products = [
-    {id:1, title:"Rose",description:"Valentine flower",quantity:500, unitprice:12},
-    {id:2, title:"Gerbera",description:"Wedding flower",quantity:1500, unitprice:16},
-    {id:3, title:"Lotus",description:"Worship flower",quantity:5000, unitprice:17},
-    {id:4, title:"Lily",description:"Smelling flower",quantity:400, unitprice:25},
-    {id:5, title:"Marigold",description:"Festival flower",quantity:7500, unitprice:34},
-  ];
-  res.send(products);
-};
+var app = exp();
 
 
-// setting router to map requests  with controllers
-app.get ('/products',productController);
-var server = app.listen(7667, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://localhost:7667", host, port)
-})
+var con = mysql.createConnection({
+
+	host:"localhost",
+	user : "root",
+	password :"root",
+	database : "test"
+
+});
+
+con.connect(function(err){
+	if(!err)
+		console.log("Connection established");
+	else
+		console.log("Connection failed");
+
+});
+
+app.listen(9000,function(){
+	console.log("server running at 9000");
+});
+
+
+
+ app.get('/',function(req,res){
+	res.sendFile(__dirname+"/index.html");
+});
+
+app.get('/emps',function(req,res){
+	con.query("select * from emp",function(err,result){
+		if(!err)
+		{
+			res.write("<table border ='1'>");
+			result.forEach(function(v){
+				
+				res.write("<tr>");
+				res.write("<td>"+v.EMPNO+"</td>");
+				res.write("<td>"+v.ENAME+"</td>");
+				res.write("</tr>");
+
+			});
+			res.write("</table>");
+			res.end();
+		}	
+	});
+});
