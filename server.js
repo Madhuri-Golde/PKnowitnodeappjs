c6e5bbf80c0dc0001851db5a9714e016572e7e77
Madhuri-Golde <madhurigolde95@gmail.com>
2023-02-05 19:33:27 +0530

    server data changes

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 var express = require('express');
-var path=require('path');
 var app = express();
 
-//server configuration
-app.use(express.static(path.join(__dirname,'public')));
+//Reusable controllers
+var productController=function(req, res){
+  console.log("Invoking  rest api for product list");
+  var products = [
+    {id:1, title:"Rose",description:"Valentine flower",quantity:500, unitprice:12},
+    {id:2, title:"Gerbera",description:"Wedding flower",quantity:1500, unitprice:16},
+    {id:3, title:"Lotus",description:"Worship flower",quantity:5000, unitprice:17},
+    {id:4, title:"Lily",description:"Smelling flower",quantity:400, unitprice:25},
+    {id:5, title:"Marigold",description:"Festival flower",quantity:7500, unitprice:34},
+  ];
+  res.send(products);
+};
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(___dirname + '/index.html'));
- });
 
-
-app.get('/hello', function (req, res) {
-    var person={firstName:'Ravi',lastName:'Tambade',age:47};
-    res.send(person);
-});
-
-var server = app.listen(7667, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Example app listening at http://localhost:7667", host, port)
-});
+// setting router to map requests  with controllers
+app.get ('/products',productController);
+var server = app.listen(8086, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://localhost:7667", host, port)
+})
